05be18ca6e93e4b014149a27bfcc19b05c2612cc
Mehrab <mmehrab.pk@gmail.com>
2026-06-16 13:37:46 +0330

    login completed

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -12,17 +12,42 @@ export async function registerUser(formData) {
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify(body)
+      body: JSON.stringify(body),
     });
     const data = await response.json();
     if (!response.ok) {
-    console.log(response);
+      console.log(response);
       throw new Error(data?.error?.message || "Registration failed");
-      
     }
     console.log(data);
     return data;
   } catch (error) {
-    throw error
+    throw error;
+  }
+}
+
+export async function loginUser(formData) {
+  const API_URL = process.env.NEXT_PUBLIC_API_URL;
+  const LOGIN_URL = `${API_URL}/api/auth/local`;
+  const body = {
+    identifier: formData.identifier,
+    password: formData.password
+  };
+  try {
+    const response = await fetch(LOGIN_URL,{
+      method: "POST",
+      headers:{
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(body)
+    })
+    const data = await response.json()
+    if(!response.ok){
+      throw new Error(data?.error?.message || "Registration failed");
+    }
+    console.log(data);
+    return data;
+  } catch (error) {
+    throw error;
   }
 }
